fbd8e01e30ad23ab5852fe4a524e4e5a096bf93f
Mykola <kolanuch13@gmail.com>
2022-12-09 10:30:32 +0200

    Hard night.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,38 @@
-import mobileMenu from './js/mobile-menu';
-mobileMenu();
-
 import { fetchCocktails } from './js/fetchCocktails.js';
+import CoctailApiService from './js/fetch';
+import templateFunction from './templates/card.hbs';
+import Pagination from 'tui-pagination'; 
+
+const myRequest = new CoctailApiService();
 
 const refs = {
   searchForm: document.querySelector('#search-form'),
   searchInput: document.querySelector('.search-input'),
   searchBtn: document.querySelector('.search-button'),
+  container: document.querySelector('.coctails__list'),
+  tui: document.querySelector('#tui-pagination-container')
 };
+
+const instance = new Pagination(refs.tui, {
+  number: 6,
+});
+
+instance.getCurrentPage();
 
 refs.searchForm.addEventListener('submit', onSearchForm);
 
 function onSearchForm(event) {
   event.preventDefault();
   refs.searchInput.innerHTML = '';
-  page = 1;
   const query = event.currentTarget.searchQuery.value.trim();
+  myRequest.searchQuery = query;
+  myRequest.page = 1;
+  myRequest.fetchCoctails()
+    .then(data => {
+      //  refs.container.innerHTML = templateFunction(data.drinks);
+    })
 
-  fetchCocktails(query).then(data => {
-    console.log(data.drinks);
-  });
+  // fetchCocktails(query).then(data => {
+  //   refs.container.innerHTML = templateFunction(data.drinks);
+  // });
 }
