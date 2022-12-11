84dbe807fa6a390c8b6f14a4e3c05ac4731c9ca0
Val Dzhadan <dzhadan.valentyna.w@gmail.com>
2022-12-11 10:29:19 +0200

    Final changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { fetchCocktails } from './js/fetchCocktails.js';
 import templateFunction from './templates/card.hbs';
+import templateCocktailFunction from './templates/modal-cocktails.hbs';
+import templateIngredientFunction from './templates/modal-ingredients.hbs';
+
+import modalCocktails from './js/modal-cocktails.js';
 
 const refs = {
   searchForm: document.querySelector('#search-form'),
   searchInput: document.querySelector('.search-input'),
   searchBtn: document.querySelector('.search-button'),
-  container: document.querySelector('.coctails__list')
+  container: document.querySelector('.coctails__list'),
 };
 
 refs.searchForm.addEventListener('submit', onSearchForm);
@@ -17,6 +21,6 @@ function onSearchForm(event) {
   const query = event.currentTarget.searchQuery.value.trim();
 
   fetchCocktails(query).then(data => {
-    refs.container.innerHTML = templateFunction(data.drinks)
+    refs.container.innerHTML = templateFunction(data.drinks);
   });
 }
