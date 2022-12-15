cb4fe4715606f3a339d387a3d419475e8cbaafe5
Mykola <112721616+kolanuch13@users.noreply.github.com>
2022-12-15 13:30:06 +0200

    Go to annother computter.

--- src/js/dropdown_menu.js
+++ src/js/dropdown_menu.js
@@ -1,28 +1,6 @@
-const refs = {
-  customSelect: document.querySelector('.select'),
-  dropDown: document.querySelector('.select__dropdown'),
-  listMobi: document.querySelector('.hero-list__mobi'),
-  selectText: document.querySelector('.input-text'),
-  svgHero: document.querySelector('.hero__icon'),
-} 
+const button = document.querySelector('.mobile-dropdown-button');
+const menu = document.querySelector('.mobile-dropdown-menu');
 
-refs.customSelect.addEventListener('click', onClickCustomSelect);
-
-function onClickCustomSelect() {
-  refs.dropDown.classList.toggle('is-hidden');
-}
-
-refs.customSelect.classList.remove('change-bgcolor');
-refs.selectText.classList.remove('change-color');
-refs.svgHero.classList.remove('change-icon-color');
-
-
-function onClickList(event) {
-  refs.customSelect.classList.add('change-bgcolor');
-  refs.selectText.classList.add('change-color');
-  refs.svgHero.classList.add('change-icon-color');
-  const result = event.target.dataset.value;
-  refs.selectText.textContent = result;
-  refs.dropDown.classList.toggle('is-hidden');
-  return onClickBtn(event);
-}
+button.addEventListener('click', event => {
+	menu.classList.toggle("is-hidden");
+})
