e716575df0cdf42b2853692a2cef0fd72c27596e
Mykola <kolanuch13@gmail.com>
2022-12-22 14:22:49 +0200

    Add some new features.

--- src/js/modal-ingredients.js
+++ src/js/modal-ingredients.js
@@ -17,17 +17,37 @@ export default modalIngredients = () => {
     refs.backdropIngr.classList.add('is-win-hidden');
   }
 
-  refs.bottomBtn.forEach(el => {
-    el.addEventListener('click', toggleBottomBtn);
-  });
-
-  function toggleBottomBtn() {
+  let arrFavoriteIngredients = [];
+  if (localStorage.getItem("favoriteIngredients") !== null) {
+    arrFavoriteIngredients = localStorage.getItem("favoriteIngredients").split(",");
     refs.bottomBtn.forEach(el => {
-      if (el.textContent === 'Add to favorite') {
-        el.textContent = 'Remove from favorite';
+      const idIngredients = el.dataset.id;
+      if (arrFavoriteIngredients.includes(idIngredients)) {
+        el.innerHTML = (`beforeend`, `<span>Remove from favorite</span>`);
       } else {
-        el.textContent = 'Add to favorite';
+        el.innerHTML = (`beforeend`, `<span>Add to favorite</span`);
       }
-    });
+    })
   }
+
+  refs.bottomBtn.forEach(el => {
+    el.addEventListener('click', (event) => {
+      // Add to lockal storage
+      const idIngredients = event.currentTarget.dataset.id;
+      if (!arrFavoriteIngredients.includes(idIngredients)) {
+        arrFavoriteIngredients.push(idIngredients);
+        localStorage.setItem("favoriteIngredients", arrFavoriteIngredients);
+      } else {
+        arrFavoriteIngredients.splice(arrFavoriteIngredients.indexOf(idIngredients), 1);
+        localStorage.setItem("favoriteIngredients", arrFavoriteIngredients);
+      }
+      // Check if it in there
+      let storFavorite = localStorage.getItem("favoriteIngredients");
+      if (storFavorite.includes(idIngredients)) {
+        el.innerHTML = (`beforeend`, `<span>Remove from favorite</span>`);
+      } else {
+        el.innerHTML = (`beforeend`, `<span>Add to favorite</span`);
+      }
+    })
+  });
 };
